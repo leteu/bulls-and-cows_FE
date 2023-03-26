b0cf716a0a2fa1eb52d9247bd7730d3a6f576839
ghshin <leteu610@gmail.com>
2023-03-26 21:24:36 +0900

    Fix: 폴더 구조 및 alias 정리

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      types: resolve(__dirname, './types'),
+      app: resolve(__dirname, '.'),
+      src: resolve(__dirname, './src'),
+    }
+  },
+  define: {
+    'process.env': {},
+    global: {},
+  },
 })
