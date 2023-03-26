6f5ca85b8b3ae221bd8d1d2b8884590c5edd1596
ghshin <leteu610@gmail.com>
2023-03-26 21:25:43 +0900

    Feat: 버튼 컴포넌트 개발

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="btn"
+    :class="{
+      'btn__primary': !secondary,
+      'btn__secondary': secondary,
+    }"
+  ><p>{{ label }}</p></div>
+</template>
+
+<script lang="ts" setup>
+import "./style.scss"
+
+defineProps({
+  label: {
+    type: String,
+    default: () => '',
+  },
+  secondary: {
+    type: Boolean,
+    default: () => false,
+  }
+})
+</script>
---
+++ src/components/Button/index.ts
@@ -0,0 +1,1 @@
+export { default as BacBtn } from './Button.vue'
