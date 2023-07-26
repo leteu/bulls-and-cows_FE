490458bd4da39f1a24ae54edc151699e5dff6f74
ghshin <leteu610@gmail.com>
2023-07-26 11:50:51 +0900

    Feat: Dialog Wrapper

---
+++ src/components/Dialog/DialogWrap.vue
@@ -0,0 +1,24 @@
+<template>
+  <Transition
+    name="custom-classes"
+    enter-active-class="animate__animated animate__fadeIn"
+    leave-active-class="animate__animated animate__fadeOut"
+    appear
+  >
+    <div class="dialog" v-if="show">
+      <slot></slot>
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts" setup>
+import 'animate.css';
+import './style.scss'
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  }
+})
+</script>
