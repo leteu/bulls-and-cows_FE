fa578fe8390e6bb782b6571bd587cafd57df5824
ghshin <leteu610@gmail.com>
2023-07-26 11:50:26 +0900

    Feat: Separator component

---
+++ src/components/misc/Separator.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="styles" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  vertical: {
+    type: Boolean,
+    default: false,
+  },
+  inset: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+const orientation = computed(() => props.vertical ? 'vertial' : 'horizontal')
+const inset = computed(() => props.inset ? '-inset' : '')
+
+const styles = computed(() => `separator separator--${orientation.value}${inset.value}`)
+</script>
+
+<style lang="scss">
+.separator {
+  border: 0;
+  background: #dadada;
+  margin: 0;
+  transition: background .3s, opacity .3s;
+  flex-shrink: 0;
+
+  &--horizontal {
+    display: block;
+    height: 1px;
+
+    &-inset {
+      margin-left: 16px;
+      margin-right: 16px;
+    }
+  }
+
+  &--vertical {
+    width: 1px;
+    height: auto;
+    align-self: stretch;
+  
+    &-inset {
+      margin-top: 8px;
+      margin-bottom: 8px;
+    }
+  }
+}
+</style>
