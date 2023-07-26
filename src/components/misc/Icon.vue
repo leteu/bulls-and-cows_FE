81144e2cc58d166288584bd34816ec12a006f2ce
ghshin <leteu610@gmail.com>
2023-07-26 11:52:37 +0900

    Chore: material icon and Icon component

---
+++ src/components/misc/Icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <i class="material-icons">{{ name }}</i>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
--- src/components/misc/index.ts
+++ src/components/misc/index.ts
@@ -1,1 +1,2 @@
 export { default as Separator } from './Separator.vue'
+export { default as Icon } from './Icon.vue'
